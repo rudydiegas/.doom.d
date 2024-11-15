e31d1a514dc965d8ddf2f2026854a94d6c8b77d4
rsorense13 <rsorense@andrew.cmu.edu>
2024-11-15 07:05:46 -0500

    First flushed-out config

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("8d412c0ed46b865312d6df5c1dfd1821d349dd3cba00049cf88c4ad34403597e" "9f986dcc0de26c1c8b6dfd749eb7351b1a3c8db31b3330a7dfdd25be1b47cb22" "4edad12267c88bb57aab5a5c0d2e23740c6f552b6a36fb785dfb4e4725808eab" default))
+ '(warning-suppress-types '((initialization) (defvaralias) (lexical-binding))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
